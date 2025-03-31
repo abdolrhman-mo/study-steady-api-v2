3a868ba4dc3c694e4aed42f7d2d3c92592def61f
abdolrhman-mo <abdelrhamnlearn@gmail.com>
2025-03-31 12:19:01 +0200

    Added today_sessions to sessions api

--- session/views.py
+++ session/views.py
@@ -1,3 +1,5 @@
+from django.utils import timezone
+from rest_framework.response import Response
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import IsAuthenticated
 
@@ -11,10 +13,31 @@ class SessionView(ListCreateAPIView):
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        return Session.objects.filter(user=self.request.user)[:50]
+        return Session.objects.filter(user=self.request.user).order_by('-created_at')[:50]
     
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        
+        # Filter today's sessions
+        today = timezone.now().date()
+        today_sessions = [session for session in queryset if session.created_at.date() == today]
+        print(today_sessions)
+
+        # Serialize today's sessions
+        today_sessions_serializer = self.get_serializer(today_sessions, many=True)
+
+        # Today total minutes
+        today_total = sum(session.duration for session in today_sessions)
+
+        return Response({
+            "sessions": serializer.data,
+            "today_sessions": today_sessions_serializer.data,
+            "today_total": today_total,
+        })
 
 
 """ 
